0bfb62f92ed863cbb077cd16d02e60007737796e
Oleh-Tsvietkov <tsvietkov.oleh@gmail.com>
2021-11-19 21:48:25 +0200

    CypressHW01_1

--- cypress/integration/CypressOTS_HW01.spec.js
+++ cypress/integration/CypressOTS_HW01.spec.js
@@ -6,20 +6,17 @@ describe('Oleh\'s First Test', () => {
     cy.url().should('include', '/#/login')
   })
 
-
   it('Fills Email for Sign In ', () => {
     cy.get(':nth-child(1) > .form-control')
       .type('qwe@1.com')
       .should('have.value', 'qwe@1.com')
   })
 
-
   it('Fills Passowrd for Sign In ', () => { 
    cy.get(':nth-child(2) > .form-control')
      .type('Qwerty12')
      .should('have.value', 'Qwerty12')
   })
-
 
   it('Clicks Sign In bttn ', () => { 
    cy.get('.btn')
