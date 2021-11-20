65a678a25ef017b78ba569ccc3094c6b3366c0d9
Oleh-Tsvietkov <tsvietkov.oleh@gmail.com>
2021-11-20 13:31:03 +0200

    CypressHW_ADV_1

--- cypress/integration/CypressOTS_HW01.spec.js
+++ cypress/integration/CypressOTS_HW01.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="Cypress" />
 
+
+// BASIC HW Cypress-1
+
 describe('Oleh\'s First Test', () => {
   it('Visits Conduit Sign In on Local ', () => {
     cy.visit('http://localhost:1667/#/login')
@@ -32,3 +35,52 @@ describe('Oleh\'s First Test', () => {
    })
 })
 
+// ADVANCED HW Cypress-1
+
+
+describe('Oleh\'s Advanced Test', () => {
+  let user;
+  
+  before(function () {
+    cy.task("freshUser").then((object) => {
+        user = object;
+        user.password += 'Q1q';
+    })
+  })
+
+  it('Visits Conduit Sign Up on Local ', () => {
+    cy.visit('http://localhost:1667/#/register')
+    cy.url().should('include', '/#/register')
+    })
+
+  it('Fills Username for Sign Up ', () => { 
+    cy.get(':nth-child(1) > .form-control')
+      .type(user.username)
+      .should('have.value', user.username)
+    })
+
+  it('Fills Username for Sign Up ', () => { 
+    cy.get(':nth-child(2) > .form-control')
+      .type(user.email)
+      .should('have.value', user.email)
+    })
+
+   it('Fills Username for Sign Up ', () => { 
+     cy.get(':nth-child(3) > .form-control')
+       .type(user.password)       
+     })
+
+   it('Clicks Sign Up bttn ', () => { 
+     cy.get('.btn')
+       .contains('Sign up')
+       .click()
+   })
+   
+  it('Asserts logged in User\'s username ', () => { 
+    cy.get(':nth-child(4) > .nav-link')
+      .contains(user.username)
+      .should('exist')
+    cy.get('.swal-button')
+      .click()
+   })
+})
